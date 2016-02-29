a6e9d650ae465c15de4715eb902a7d6de8daae7d
manducku <alwaysfun2183@gmail.com>
2016-02-29 21:58:41 +0900

    직사각형으로 배열하는 함수 추가

--- JS/book/simplegallery/simplegallay.js
+++ JS/book/simplegallery/simplegallay.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 
-	$(".align-button").click(function(){
+	$(".vertical-align").click(function(){
 	
 	var $images = $("#container img");
 	var length = $images.length;
@@ -13,5 +13,31 @@ $(document).ready(function(){
 		$images.eq(i).css({left:x});
 		}
 	});
+
+
+// 직사각형으로 그림 배열하기 
+
+	$(".rec-align").click(function(){
+		var $images = $("#container img");
+		var length = $images.length;
+		position = $images.offset();
+		var x = 0;
+		var y = 0;
+		console.log(x, y);
+		for(var i = 1; i< length; i++){
+			if(i%3 === 0)
+			{
+				x = 0;
+				y = y+200;
+				$images.eq(i).css({left: x, top:y});
+				console.log(x, y);
+				continue;
+			}
+				 x = x+ 200;
+				$images.eq(i).css({left:x, top: y});
+		}
+	});
+
+
 
 });
