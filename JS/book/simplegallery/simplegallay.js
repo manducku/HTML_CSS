c58f21369773e30072a6293e605df5d0fe801b1c
manducku <alwaysfun2183@gmail.com>
2016-03-01 16:25:51 +0900

    대각선으로 배열함수 추가 및 애니메이션 효과 추가

--- JS/book/simplegallery/simplegallay.js
+++ JS/book/simplegallery/simplegallay.js
@@ -2,17 +2,18 @@
 
 $(document).ready(function(){
 
-	
 	var $images = $("#container img");
 	var length = $images.length;
+	
+	$(".reset").click(function(){
+		reset();
+	});
 
 // 위치 초기화 함수 
 	var reset = function(){
-		$(".reset").click(function(){
 		for(i = 0; i < length; i++){
 			$images.eq(i).css({left:0, top: 0});
 			}
-		});
 	};
 
 // 수평으로 그림 배열하기 
@@ -22,12 +23,9 @@ $(document).ready(function(){
 		for(var i = 1; i < length; i++){
 			var $img = $images.eq(i);
 			x = x + 200;
-			y = 0;
-			// 여기서 0이 필요한 이유를 확실하게 모르겠음. 
-			$images.eq(i).css({left:x ,top: 0});
+			$images.eq(i).animate({left:x}, 2000);
 			}
 	});
-
 
 // 직사각형으로 그림 배열하기 
 	$(".rec-align").click(function(){
@@ -39,13 +37,31 @@ $(document).ready(function(){
 			{
 				x = 0;
 				y = y+200;
-				$images.eq(i).css({left: x, top:y});
+				$images.eq(i).animate({left: x, top:y}, 2000);
 				continue;
 			}
 				 x = x+ 200;
-				$images.eq(i).css({left:x, top: y});
+				$images.eq(i).animate({left:x, top: y}, 2000);
 		}
 	});
+
+	$(".animate").click(function(){
+
+		reset();
+		var x = 0,
+			y = 0;
+
+		for(var i  = 1; i < length; i++){
+			x = x + 100;
+			y = y + 100;
+		$images.eq(i).animate({left: x,top:y}, 1000);
+	}
+
+
+
+	});
+
+
 
 
 });
