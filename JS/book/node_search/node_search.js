426e26c8e8f9ca1d8570f248e607129c4927fe7a
manducku <alwaysfun2183@gmail.com>
2016-03-02 01:29:01 +0900

    특정 노드에 접근하는 방법 추가

--- JS/book/node_search/node_search.js
+++ JS/book/node_search/node_search.js
@@ -17,4 +17,27 @@ $(document).ready(function(){
 	//속성으로 노드 찾기 
 	$("[class]").css("border", "4px solid #999999");
 	$("[class*=test]").css("border", "4px solid #FFCCCC");
+
+	//찾은 노드 개수 구하기 
+	console.log("length= "+$("div").length);
+
+	// li 2번째 노드에 접근 
+	$("li").eq(1).css("background-color", "#FF3333");
+
+	// DOM 객체 리턴 받아서 스타일 변경하기
+	$("li").get(2).style.background = "#FF6600";
+	$("li")[3].style.background = "#CC9933";
+
+	// each 메소드에서 index 확인하기, 매개 변수로 index를 넘겨주므로 확인할 수 있음 
+	$("li").each(function(index){
+		console.log("index= "+ index);
+	});
+
+	//메뉴 노드(ul.menu li)의 border 속성을 2배수로 점점 두껍게 만들기
+	$("ul.menu li").each(function(index){
+		var border = index*5;
+		$(this).css("border",border+"px solid black");
+	});
+	
 });
+
