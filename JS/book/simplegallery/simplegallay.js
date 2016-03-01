0a78699ad48be7b8ffe225e64d35b1dff9ba65c0
manducku <alwaysfun2183@gmail.com>
2016-03-01 15:02:50 +0900

    reset 버튼 추가

--- JS/book/simplegallery/simplegallay.js
+++ JS/book/simplegallery/simplegallay.js
@@ -2,52 +2,49 @@
 
 $(document).ready(function(){
 
-	$(".vertical-align").click(function(){
 	
 	var $images = $("#container img");
 	var length = $images.length;
 
-	for(var i = 0; i < length; i++){
-		var $img = $images.eq(i);
-		var x = i*200;
-		$images.eq(i).css({left:x});
-		}
+// 위치 초기화 함수 
+	var reset = function(){
+		$(".reset").click(function(){
+		for(i = 0; i < length; i++){
+			$images.eq(i).css({left:0, top: 0});
+			}
+		});
+	};
+
+// 수평으로 그림 배열하기 
+	$(".vertical-align").click(function(){
+		reset();
+		var x = 0, y = 0;
+		for(var i = 1; i < length; i++){
+			var $img = $images.eq(i);
+			x = x + 200;
+			y = 0;
+			// 여기서 0이 필요한 이유를 확실하게 모르겠음. 
+			$images.eq(i).css({left:x ,top: 0});
+			}
 	});
 
 
 // 직사각형으로 그림 배열하기 
-
 	$(".rec-align").click(function(){
-		var $images = $("#container img");
-		var length = $images.length;
-		position = $images.offset();
-		var x = 0;
-		var y = 0;
-		console.log(x, y);
+		reset();
+		var x = 0, y = 0;
 		for(var i = 1; i< length; i++){
+			var $img = $images.eq(i);
 			if(i%3 === 0)
 			{
 				x = 0;
 				y = y+200;
 				$images.eq(i).css({left: x, top:y});
-				console.log(x, y);
 				continue;
 			}
 				 x = x+ 200;
 				$images.eq(i).css({left:x, top: y});
 		}
-	});
-
-
-	$(".reset").click(function(){
-
-		var $images = $("#container img");
-		var length = $images.length;
-
-		for(i = 0; i < length; i++){
-			$images.eq(i).css({left:0, top: 0});
-		}
-
 	});
 
 
